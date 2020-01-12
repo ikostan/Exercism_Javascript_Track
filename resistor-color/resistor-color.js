19a73bc8c14acef020868d871b19a3454416b45c
Egor Kostan <igorkostan@gmail.com>
2020-01-11 19:14:17 -0800

    Update resistor-color.js
    Resistor Color (JS) -> done

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -3,8 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+  return COLORS.indexOf(color.toLowerCase())
 };
 
-export const COLORS = undefined;
+//
+// Mnemonics map the colors to the numbers, that, 
+// when stored as an array, happen to map to their 
+// index in the array: 
+// Better Be Right Or Your Great Big Values Go Wrong.
+//
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"];
